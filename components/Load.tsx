bf9d24283a6e477c606b6c648aeeb69e751143a8
th-release <th.release.cth@gmail.com>
2022-09-30 16:40:57 +0900

    feat: Loading Page

--- components/Load.tsx
+++ components/Load.tsx
@@ -2,7 +2,7 @@ import LoadStyles from '../styles/Load.module.css'
 
 const Load = () => {
   return (
-    <div>
+    <div className={LoadStyles.main}>
       <div className={LoadStyles.logoIcon}></div>
       <div className={LoadStyles.title}>PROTECTED ANIMAL</div>
     </div>
