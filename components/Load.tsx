ab74b62953f30ab7f23ae626e1b0c450b28560bf
th-release <th.release.cth@gmail.com>
2022-10-03 22:37:18 +0900

    feat: Loading Page

--- components/Load.tsx
+++ components/Load.tsx
@@ -4,7 +4,7 @@ const Load = () => {
   return (
     <div className={LoadStyles.main}>
       <div className={LoadStyles.logoIcon}></div>
-      <div className={LoadStyles.title}>PROTECTED ANIMAL</div>
+      <div className={LoadStyles.title}>대충 만든 로딩페이지</div>
     </div>
   )
 }
