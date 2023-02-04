e46d09fc11444d8205b22ebc0c6e043c4bc55e91
th-release <cth.release@gmail.com>
2023-02-04 20:46:16 +0900

    feat: Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,13 @@
-export {};
+import { Head } from '@/components/Head';;
+import type { AppProps } from 'next/app';
+import { GlobalStyle } from '@/styles/globals';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Head />
+      <Component {...pageProps} /> 
+    </>
+  )
+}
